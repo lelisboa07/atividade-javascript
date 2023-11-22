2b7a1695a86541b993ac0d091d20290aa4274a73
Letícia Lisboa <133930947+lelisboa07@users.noreply.github.com>
2023-11-22 15:32:58 -0300

    Atividades

---
+++ atividade.js
@@ -0,0 +1,63 @@
+// ATIVIDADE 1
+
+// let nome
+// let cidade
+
+// console.log(typeof nome)
+// console.log(typeof cidade)
+
+// nome = prompt("Insira seu nome")
+// cidade = prompt("Insira sua cidade")
+
+// console.log(nome)
+// console.log(cidade)
+
+// console.log(typeof nome)
+// console.log(typeof cidade)
+
+// console.log(` Olá, meu nome é ${nome} e eu moro em ${cidade}.`)
+
+
+
+// ATIVIDADE 2
+
+// let pergunta1 = prompt("Você gosta de programar?")
+// let pergunta2 = prompt("Você já viajou para fora do país?")
+// let pergunta3 = prompt("Você tem animais de estimação?")
+
+// console.log("Você gosta de programar?")
+// console.log(pergunta1)
+
+// console.log("Você já viajou para fora do país?")
+// console.log(pergunta2)
+
+// console.log("Você tem animais de estimação?")
+// console.log(pergunta3)
+
+
+
+// ATIVIDADE 3
+
+// let x = 4
+// let y = 10
+
+// console.log("x =", x, "y =", y)
+
+// let novoX = y
+// let novoY = x
+
+// console.log("Novo x =", novoX, "Novo y =", novoY)
+
+
+
+// ATIVIDADE 4
+
+// let valor1 = Number(prompt("Insira um valor"))
+// let valor2 = Number(prompt("Insira outro valor"))
+
+// let soma = valor1 + valor2
+// console.log("A soma dos números é:", soma)
+
+// let multiplicacao = valor1 * valor2
+// console.log("A multiplicação dos números é:", multiplicacao)
+
